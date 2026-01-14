417e9b1f2706931706e206d2b59be05d218e319e
dhanasuriya <d_suriya@outlook.com>
2026-01-14 14:45:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="suriys",
+    author_email="d_suriya@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
